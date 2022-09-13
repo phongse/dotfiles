8f4b8c57d9e15982c80260ebf0fa6021279d460a
Phong Tran <540580+phongse@users.noreply.github.com>
2022-09-13 04:43:10 +0200

    remove whitespace and minor change

--- hyprlandwrapper.sh
+++ hyprlandwrapper.sh
@@ -6,6 +6,6 @@ export SDL_VIDEODRIVER=wayland
 export MOZ_ENABLE_WAYLAND=1
 export QT_QPA_PLATFORM=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
-export XCURSOR_SIZE=32
+export XCURSOR_SIZE=24
 
 exec Hyprland &> /dev/null
